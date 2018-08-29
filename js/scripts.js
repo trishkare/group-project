1f0406bf0b04a557e937bac74ab35aa5a6d9a419
resort <rosemaryashioya>
2018-08-29 16:02:08 +0300

    Add changes made on group-project.html

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $("blanks form").submit(function(event) {
+    var blanks = ["name", "Address", "Qualifications"];
+    blanks.foreach(function(blank) {
+    
+    })
+  })
+})
